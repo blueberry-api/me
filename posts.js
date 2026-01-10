ce3c1a530f59f4ef2725be5e149abb038bbdc9fe
cheral <2111413873@qq.com>
2026-01-10 22:50:45 +0800

    2026.1.10

--- posts.js
+++ posts.js
@@ -1,5 +1,5 @@
 // 文章列表 - 写一篇就在这里加一条
 const posts = [
-    { title: "第一篇博客", date: "2026-01-09", file: "first-post.md" },
+    { title: "2026的意象", date: "2026-01-10", file: "2026的意象.md" },
     // { title: "新文章标题", date: "2026-01-10", file: "new-post.md" },
 ];
